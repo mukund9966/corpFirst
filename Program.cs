0d31cba35580e5f554bf11f132d62369601acbda
mukund9966 <khandelwalmukund20@gmail.com>
2023-07-11 12:13:33 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class newClass {
+        public string newFunc(string sname, string ename)
+        {
+            return sname + "  " + ename;
+        } }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            newClass nc = new newClass();
+            Console.WriteLine(nc.newFunc("mukund", "khandelwal"));
+            Console.ReadKey();
+
+        }
+    }
+}
